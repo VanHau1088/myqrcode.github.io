aa4776345ad949ba4c020fa01acdbae562c1b3b7
Van Hau <hn4901298@gmail.com>
2024-12-08 17:34:55 +0700

    Resolve merge conflicts

--- JS/index.js
+++ JS/index.js
@@ -3,11 +3,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const title = queryParams.get('title');
     const userText = queryParams.get('text');
     if(title){
-      document.querySelector('title').textContent = title;
+      document.querySelectorAll('title').textContent = title;
     }
 
     if (userText) {
-      document.querySelector('userText').textContent = userText;
+      document.querySelectorAll('userText').textContent = userText;
     }
   });
 
