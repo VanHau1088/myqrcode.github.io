ece2f88f2968e1c5ca897dae5de8ddb9d061d6d4
Van Hau <hn4901298@gmail.com>
2024-12-08 17:58:11 +0700

    Resolve merge conflicts

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const queryParams = new URLSearchParams(window.location.search);
-  const title = queryParams.title('title')
+  const title = queryParams.get('title');
   const userText = queryParams.get('text');
  if(title) {
   document.querySelector('title').textContent = title;
