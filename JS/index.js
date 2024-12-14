ffc435dd11837b99b1a748bfd26237d5b480585e
Van Hau <hn4901298@gmail.com>
2024-12-14 17:21:18 +0700

    Cập nhật trang landing page và mã QR

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,26 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  const queryParams = new URLSearchParams(window.location.search);
-  const userText = queryParams.get('text');
-  if (userText) {
-    document.getElementById('userText').textContent = userText;
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   const queryParams = new URLSearchParams(window.location.search);
+//   const userText = queryParams.get('text');
+//   if (userText) {
+//     document.getElementById('userText').textContent = userText;
+//   }
+// });
+
+// index.js
+document.addEventListener('DOMContentLoaded', () => {
+  const params = new URLSearchParams(window.location.search);
+  const shortUrl = params.get('text');
+  if (shortUrl) {
+    fetch(`http://localhost:3000/${shortUrl}`)
+      .then(response => response.text())
+      .then(text => {
+        document.getElementById('userText').innerText = decodeURIComponent(text);
+      })
+      .catch(error => {
+        document.getElementById('userText').innerText = 'Error loading content';
+      });
+  } else {
+    document.getElementById('userText').innerText = 'No content available';
   }
 });
+
